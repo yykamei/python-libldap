c9d8c95f5487c1535ed5e91c502e29532f19977a
Yutaka Kamei <kamei@ykamei.net>
2015-07-25 00:15:05 +0900

    Modify directory tree

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages, Extension
 
 ext_module = Extension('_libldap',
-                       sources=['src/_libldap.c',
-                                'src/_libldap_utils.c',
-                                'src/_libldap_bind.c',
-                                'src/_libldap_search.c',
-                                'src/_libldap_result.c',
-                                'src/_libldap_add.c',
+                       sources=['Modules/_libldap.c',
+                                'Modules/_libldap_utils.c',
+                                'Modules/_libldap_bind.c',
+                                'Modules/_libldap_search.c',
+                                'Modules/_libldap_result.c',
+                                'Modules/_libldap_add.c',
                                 ],
                        libraries=['ldap'],
                        extra_compile_args=['-g', '-O0'])
@@ -18,7 +18,7 @@ setup(name='libldap',
       version='0.0.4',
       description='A Python binding for libldap',
       ext_modules=[ext_module],
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
+      packages=find_packages('Lib'),
+      package_dir={'': 'Lib'},
       py_modules=['libldap'],
 )
