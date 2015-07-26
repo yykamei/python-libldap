80470a2db68fb2220231addd5fb2f1132edaecb4
Yutaka Kamei <kamei@ykamei.net>
2015-07-26 18:38:41 +0900

    Fix add malloc problem?

--- Modules/_common.c
+++ Modules/_common.c
@@ -19,103 +19,6 @@ _XDECREF_MANY(PyObject **objs[], size_t count)
 	}
 }
 
-static LDAPMod *pair2LDAPMod(PyObject *key, PyObject *value, int mod_op);
-static struct berval *str2berval(PyObject *str);
-static void freeLDAPMods(LDAPMod **mods);
-static void freeLDAPMod(LDAPMod *mod);
-
-
-LDAPMod **
-dict2LDAPMods(PyObject *dict)
-{
-	LDAPMod **mods;
-	Py_ssize_t pos = 0, len;
-	PyObject *key = NULL, *value = NULL, *mod_value = NULL;
-	int mod_op;
-
-	if (!PyDict_Check(dict)) {
-		XDECREF_MANY(&dict);
-		PyErr_SetString(PyExc_ValueError, "Object MUST be dict type");
-		return NULL;
-	}
-
-	len = PyDict_Size(dict);
-	mods = (LDAPMod **)PyMem_RawMalloc(sizeof(LDAPMod *) * len);
-	if (mods == NULL) {
-		PyErr_NoMemory();
-		return NULL;
-	}
-
-	while (PyDict_Next(dict, &pos, &key, &value)) {
-		if (!PyUnicode_Check(key)) {
-			PyErr_SetString(PyExc_ValueError, "Each key MUST be str type");
-			return NULL;
-		}
-		if (PyList_Check(value)) {
-			mods[pos] = pair2LDAPMod(key, value, LDAP_MOD_ADD | LDAP_MOD_BVALUES);
-			if (mods[pos] == NULL) {
-				freeLDAPMods(mods);
-				return NULL;
-			}
-		} else if (PyTuple_Check(value)) {
-			if (PyTuple_GET_SIZE(value) != 2) {
-				PyErr_SetString(PyExc_ValueError, "Size of tuple MUST be two");
-				return NULL;
-			}
-			mod_op = (int)PyLong_AsLong(PyTuple_GET_ITEM(value, 1));
-			mod_value = PyTuple_GET_ITEM(value, 2);
-			mods[pos] = pair2LDAPMod(key, mod_value, mod_op | LDAP_MOD_BVALUES);
-			if (mods[pos] == NULL) {
-				freeLDAPMods(mods);
-				return NULL;
-			}
-		} else {
-			PyErr_SetString(PyExc_ValueError, "Each value MUST be list or tuple type");
-			return NULL;
-		}
-	}
-	return mods;
-}
-
-
-static LDAPMod *
-pair2LDAPMod(PyObject *key, PyObject *value, int mod_op)
-{
-	LDAPMod *mod;
-	int len;
-	Py_ssize_t i;
-	PyObject *val;
-
-	mod = (LDAPMod *)PyMem_RawMalloc(sizeof(LDAPMod));
-	if (mod == NULL) {
-		PyErr_NoMemory();
-		return NULL;
-	}
-
-	len = PyList_GET_SIZE(value);
-	mod->mod_op = mod_op;
-	mod->mod_type = PyUnicode_AsUTF8(key);
-	if (mod->mod_type == NULL) {
-		freeLDAPMod(mod);
-		PyErr_NoMemory();
-		return NULL;
-	}
-	mod->mod_bvalues = (struct berval **)PyMem_RawMalloc(sizeof(struct berval *) * len);
-	if (mod ->mod_bvalues == NULL) {
-		freeLDAPMod(mod);
-		PyErr_NoMemory();
-		return NULL;
-	}
-	for (i = 0; i < len; i++) {
-		val = PyList_GET_ITEM(value, i);
-		mod->mod_bvalues[i] = str2berval(val);
-		if (mod->mod_bvalues[i] == NULL) {
-			freeLDAPMod(mod);
-			return NULL;
-		}
-	}
-	return mod;
-}
 
 static struct berval *
 str2berval(PyObject *str)
@@ -144,35 +47,104 @@ str2berval(PyObject *str)
 }
 
 
-static void
-freeLDAPMods(LDAPMod **mods)
+static LDAPMod *
+pair2LDAPMod(PyObject *key, PyObject *value, int mod_op)
 {
-	LDAPMod **l;
-	for (l = mods; *l; l++) {
-		freeLDAPMod(*l);
+	LDAPMod *mod;
+	int len;
+	Py_ssize_t i;
+	PyObject *val;
+
+	mod = (LDAPMod *)PyMem_RawMalloc(sizeof(LDAPMod));
+	if (mod == NULL) {
+		PyErr_NoMemory();
+		return NULL;
 	}
-	PyMem_RawFree(mods);
-}
 
-
-static void
-freeLDAPMod(LDAPMod *mod)
-{
-	int i;
-	if (mod == NULL)
-		return;
-
-	if (mod->mod_bvalues == NULL) {
+	len = PyList_GET_SIZE(value);
+	mod->mod_op = mod_op;
+	mod->mod_type = PyUnicode_AsUTF8(key);
+	if (mod->mod_type == NULL) {
 		PyMem_RawFree(mod);
-		return;
+		PyErr_NoMemory();
+		return NULL;
+	}
+	mod->mod_bvalues = (struct berval **)PyMem_RawMalloc(sizeof(struct berval *) * (len + 1));
+	if (mod ->mod_bvalues == NULL) {
+		PyMem_RawFree(mod);
+		PyErr_NoMemory();
+		return NULL;
+	}
+	for (i = 0; i < len; i++) {
+		val = PyList_GET_ITEM(value, i);
+		mod->mod_bvalues[i] = str2berval(val);
+		if (mod->mod_bvalues[i] == NULL) {
+			PyMem_RawFree(mod->mod_bvalues);
+			PyMem_RawFree(mod);
+			PyErr_NoMemory();
+			return NULL;
+		}
+	}
+	mod->mod_bvalues[len] = NULL;
+	return mod;
+}
+
+
+LDAPMod **
+dict2LDAPMods(PyObject *dict)
+{
+	LDAPMod **mods;
+	Py_ssize_t pos = 0;
+	Py_ssize_t len;
+	PyObject *key = NULL;
+	PyObject *value = NULL;
+	PyObject *mod_value = NULL;
+	int i;
+	int mod_op;
+
+	if (!PyDict_Check(dict)) {
+		XDECREF_MANY(&dict);
+		PyErr_SetString(PyExc_ValueError, "Object MUST be dict type");
+		return NULL;
 	}
 
-	for (i = 0; mod->mod_bvalues[i]; i++) {
-		if (mod->mod_bvalues[i] != NULL)
-			PyMem_RawFree(mod->mod_bvalues[i]);
+	len = PyDict_Size(dict);
+	mods = (LDAPMod **)PyMem_RawMalloc(sizeof(LDAPMod *) * (len + 1));
+	if (mods == NULL) {
+		PyErr_NoMemory();
+		return NULL;
 	}
-	PyMem_RawFree(mod->mod_bvalues);
-	PyMem_RawFree(mod);
+
+	i = 0;
+	while (PyDict_Next(dict, &pos, &key, &value)) {
+		if (!PyUnicode_Check(key)) {
+			PyErr_SetString(PyExc_ValueError, "Each key MUST be str type");
+			return NULL;
+		}
+		if (PyList_Check(value)) {
+			mod_op = LDAP_MOD_ADD | LDAP_MOD_BVALUES;
+			mod_value = value;
+		} else if (PyTuple_Check(value)) {
+			if (PyTuple_GET_SIZE(value) != 2) {
+				PyErr_SetString(PyExc_ValueError, "Size of tuple MUST be two");
+				return NULL;
+			}
+			mod_op = (int)PyLong_AsLong(PyTuple_GET_ITEM(value, 1)) | LDAP_MOD_BVALUES;
+			mod_value = PyTuple_GET_ITEM(value, 2);
+		} else {
+			PyErr_SetString(PyExc_ValueError, "Each value MUST be list or tuple type");
+			return NULL;
+		}
+		mods[i] = pair2LDAPMod(key, mod_value, mod_op);
+		if (mods[i] == NULL) {
+			PyMem_RawFree(mods);
+			return NULL;
+		}
+		i++;
+	}
+	mods[len] = NULL;
+	return mods;
 }
+
 
 /* vi: set noexpandtab : */
